e62c77a5cf1dfc9b4ce436ddd536a659b6813046
Nimesh Srivastava <snimesh412@gmail.com>
2021-04-10 22:35:24 +0530

    update

--- detection.py
+++ detection.py
@@ -24,34 +24,25 @@ while True:
     encodings = face_recognition.face_encodings(image)
     names = []
     for encoding in encodings:
-       # Compare encodings with encodings in data["encodings"]
-       #Matches contain array with boolean values and True for the embeddings it matches closely
-       #and False for rest
-        matches = face_recognition.compare_faces(data["encodings"],
-         encoding)
-        #set name to unknown if no encoding matches
+        matches = face_recognition.compare_faces(data["encodings"], encoding)
         name = "Unknown"
-        # check for a match.
         if True in matches:
-            # Find and store success positions
             matchedIdxs = [i for (i, b) in enumerate(matches) if b]
             counts = {}
-            # Count frequency of all matching indices
             for i in matchedIdxs:
                 name = data["names"][i]
-                counts[name] = counts.get(name, 0) + 1
-            # set name to matched index with highest count
+                counts[name] = counts.get(name, 0) + 1            
             name = max(counts, key=counts.get)
- 
- 
-        # update the list of names
+
         names.append(name)
-        #draw the rectangles and names over detected faces in frame.
+
         for ((x, y, w, h), name) in zip(faces, names):
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
             cv2.putText(frame, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
              0.75, (0, 0, 255), 2)
+
     cv2.imshow('Frame', frame)
+    
     key = cv2.waitKey(1) &0xFF
     if key == ord('q'):
         cv2.destroyAllWindows()
